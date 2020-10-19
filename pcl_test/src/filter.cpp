f1b4c53c885c6ccb76ce79ba1dcbbf6afb682eda
Seven1996 <403784100@sjtu.edu.cn>
2020-10-19 18:22:42 +0800

    将子功能进行分解

--- pcl_test/src/filter.cpp
+++ pcl_test/src/filter.cpp
@@ -18,41 +18,40 @@ ros::Publisher pcl_pub;
 void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
 
-	ROS_INFO("hello world");
+     ROS_INFO("hello world");
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromROSMsg (*input, *cloud);
- 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+     pcl::fromROSMsg (*input, *cloud);
+     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
-	pcl::PassThrough<pcl::PointXYZ> pass;
-  pass.setInputCloud (cloud);            //设置输入点云
-  pass.setFilterFieldName ("z");         //设置过滤时所需要点云类型的Z字段
-  pass.setFilterLimits (0.2, 1.5);        //设置在过滤字段的范围
-//     pass.setFilterFieldName ("x");         //设置过滤时所需要点云类型的Z字段
-//   pass.setFilterLimits (-1.0, 1.0);        //设置在过滤字段的范围
-//       pass.setFilterFieldName ("y");         //设置过滤时所需要点云类型的Z字段
-//   pass.setFilterLimits (-1.0, 1.0);        //设置在过滤字段的范围
-  //pass.setFilterLimitsNegative (true);   //设置保留范围内还是过滤掉范围内
-  pass.filter (*cloud_filtered);  
-  pass.setInputCloud (cloud_filtered);  
-  pass.setFilterFieldName ("x");         //设置过滤时所需要点云类型的Z字段
-  pass.setFilterLimits (-0.5, 0.5);   
-  pass.filter (*cloud_filtered); 
-     
-       pass.setInputCloud (cloud_filtered);  
-  pass.setFilterFieldName ("y");       
-  pass.setFilterLimits (-0.5, 0.5);   
-  pass.filter (*cloud_filtered); 
+     pcl::PassThrough<pcl::PointXYZ> pass;
+     pass.setInputCloud (cloud);            //设置输入点云
+     pass.setFilterFieldName ("z");         //设置过滤时所需要点云类型的Z字段
+     pass.setFilterLimits (0.2, 1.5);        //设置在过滤字段的范围
+     //     pass.setFilterFieldName ("x");         //设置过滤时所需要点云类型的Z字段
+     //   pass.setFilterLimits (-1.0, 1.0);        //设置在过滤字段的范围
+     //       pass.setFilterFieldName ("y");         //设置过滤时所需要点云类型的Z字段
+     //   pass.setFilterLimits (-1.0, 1.0);        //设置在过滤字段的范围
+     //pass.setFilterLimitsNegative (true);   //设置保留范围内还是过滤掉范围内
+     pass.filter (*cloud_filtered);  
+     pass.setInputCloud (cloud_filtered);  
+     pass.setFilterFieldName ("x");         //设置过滤时所需要点云类型的Z字段
+     pass.setFilterLimits (-0.5, 0.5);   
+     pass.filter (*cloud_filtered); 
+          
+          pass.setInputCloud (cloud_filtered);  
+     pass.setFilterFieldName ("y");       
+     pass.setFilterLimits (-0.5, 0.5);   
+     pass.filter (*cloud_filtered); 
 
      sensor_msgs::PointCloud2 output;
-	//  ros::Time::
-	 output.header.stamp=ros::Time::now();
-    output.header.frame_id = input->header.frame_id;
-    pcl::toROSMsg(*cloud_filtered, output);
-    pcl_pub.publish(output);
-    std::cout<<cloud_filtered->height<<std::endl;
-    std::cout<<cloud_filtered->points[0].x<<","<<cloud_filtered->points[0].y<<","<<cloud_filtered->points[0].z<<std::endl;
-    std::cout<<cloud_filtered->points.size()<<std::endl;
+     output.header.stamp=ros::Time::now();
+     output.header.frame_id = input->header.frame_id;
+     pcl::toROSMsg(*cloud_filtered, output);
+     pcl_pub.publish(output);
+     std::cout<<cloud_filtered->height<<std::endl;
+     std::cout<<cloud_filtered->points[0].x<<","<<cloud_filtered->points[0].y<<","<<cloud_filtered->points[0].z<<std::endl;
+     std::cout<<cloud_filtered->points.size()<<std::endl;
 
 }
 
--- pcl_trajectory_generator/src/cloud_normal_calculate.cpp
+++ pcl_trajectory_generator/src/cloud_normal_calculate.cpp
@@ -1,3 +1,5 @@
+// 利用点云数据求解人体表面法向量
+
 #include <ros/ros.h>
 #include <iostream>
 #include <pcl/point_types.h>
@@ -7,12 +9,14 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
+#include <geometry_msgs/PoseArray.h>
 
 
 ros::Publisher pcl_pub;
 void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
-	ROS_INFO("hello world");
+	ROS_INFO("point cloud normal calculating");
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg (*input, *cloud);
 
@@ -29,27 +33,20 @@ void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 	pcl::PointCloud<pcl::PointXYZINormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointXYZINormal>);
 	pcl::concatenateFields(*cloud, *pcNormal, *cloud_with_normals);
 
-     sensor_msgs::PointCloud2 output;
-	//  ros::Time::
-	 output.header.stamp=ros::Time::now();
-    output.header.frame_id = "world";
-    pcl::toROSMsg(*cloud_with_normals, output);
-    pcl_pub.publish(output);
-	// pcl::io::savePCDFile("plane_cloud_out.pcd", *cloud_with_normals);
-	// ros::shutdown();
+	sensor_msgs::PointCloud2 msg;
+	msg.header.frame_id=input->header.frame_id;
+	msg.header.stamp=ros::Time::now();
+	pcl::toROSMsg(*cloud_with_normals, msg);
+    pcl_pub.publish(msg);
 }
 
 
 int main(int argc, char* argv[])
 {
-	 ros::init(argc, argv, "my_pcl_tutorial");
-     ros::NodeHandle nh;
-
- 
-     ros::Subscriber sub = nh.subscribe ("filter_output", 1, cloud_cb);
-	 pcl_pub= nh.advertise<sensor_msgs::PointCloud2> ("pcl_output", 1);
-     ros::spin();
-
- 
+	ros::init(argc, argv, "cloud_normal_calculate");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe ("cloud_preprocessed", 1, cloud_cb);
+	pcl_pub= nh.advertise<sensor_msgs::PointCloud2>("cloud_normal", 1);
+    ros::spin();
 	return 0;
 }
